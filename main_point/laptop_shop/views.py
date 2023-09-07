4706f4a8b967f51f91a2401f22e7e1d8891ea468
MuneebKhokhar786 <dev.muneeb.khokhar@gmail.com>
2023-09-07 12:25:33 -0700

    Added product index and show pages

--- main_point/laptop_shop/views.py
+++ main_point/laptop_shop/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'laptop_shop/index.html', {'products': products})
+
+def show(request, product_id):
+    try:
+        product = Product.objects.get(id=product_id)
+    except Product.DoesNotExist:
+        return render(request, 'laptop_shop/show.html', {'product': None })
