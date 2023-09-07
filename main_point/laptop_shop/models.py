b2ce284f4a8c4e35814b5ee1c22e68dd6118bd67
MuneebKhokhar786 <dev.muneeb.khokhar@gmail.com>
2023-09-07 16:23:37 -0700

    Added Collection Model

--- main_point/laptop_shop/models.py
+++ main_point/laptop_shop/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
+class Collection(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='product_images/')
+    collection = models.ForeignKey(Collection, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.name
