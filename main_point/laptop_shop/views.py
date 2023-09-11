080ecd431764cddac2033ab8d13c096baf8a6a2a
MuneebKhokhar786 <dev.muneeb.khokhar@gmail.com>
2023-09-11 07:03:43 -0700

    Added home page

--- main_point/laptop_shop/views.py
+++ main_point/laptop_shop/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from .models import Product, Collection
 
-def index(request):
+def home(request):
     products = Product.objects.all()
     collections = Collection.objects.all()
-    return render(request, 'laptop_shop/index.html', {'products': products, 'collections': collections})
+    return render(request, 'laptop_shop/home.html', {'products': products, 'collections': collections})
 
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'laptop_shop/index.html', {'products': products})
 
 def show(request, product_id):
     try:
