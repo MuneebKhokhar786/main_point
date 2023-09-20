9450f9b86ff35875fca3a9903c7423438bc7784b
MuneebKhokhar786 <dev.muneeb.khokhar@gmail.com>
2023-09-20 12:01:21 -0700

    minor change

--- main_point/laptop_shop/urls.py
+++ main_point/laptop_shop/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('products/<str:collection_name>', views.index, name='index'),
+    path('<str:collection_name>/products/', views.index, name='index'),
     path('products/<slug:product_slug>/', views.show, name='show'),
 ]
